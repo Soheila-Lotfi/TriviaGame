3f79f7aa70b795c86eb97f96705f38af9477c23e
soheila <soheilalotfi@ymail.com>
2019-06-19 16:29:11 -0400

    made the third question using jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -77,7 +77,48 @@ $("#start").click(function() {
 
         // append question and radio buttons (inputs and labels) into first form (first question)
         secondQuestion.append(question2, firstInput2, firstLabel2, secondInput2, SecondLabel2, thirdInput2, thirdLabel2);
+
+// third question
+    var question3=$("<p>").attr("class","question").html("Which English actor won the 2014 Academy Award for best actor for his role in The Theory of Everything?")
+
+    // create a form including three radio buttons and labels
+    var thirdQuestion=$("<form>");
+
+    // first radio button-first answer 
+    var firstInput3= $("<input>").attr({
+        type:'radio',
+        name:'thirdquestion',
+        value:'Colin Firth',
+        id:'first3',
+        class:'radio-button'
+    });
+    var firstLabel3=$("<label>").attr("for","first3").text("Colin Firth");
+    
+    // second radio button-second answer
+
+    var secondInput3= $("<input>").attr({
+        type:'radio',
+        name:'thirdquestion',
+        value:'Tom Hardy',
+        id:'second3',
+        class:'radio-button'
+    });
+    var SecondLabel3=$("<label>").attr("for","second3").text("Tom Hardy");
+
+    //Third radio button-Third answer
+    var thirdInput3= $("<input>").attr({
+        type:'radio',
+        name:'thirdquestion',
+        value:'Eddie Redmayne',
+        id:'third3',
+        class:'radio-button'
+    });
+    var thirdLabel3=$("<label>").attr("for","third3").text("Eddie Redmayne");
+
+    // append question and radio buttons (inputs and labels) into first form (first question)
+     thirdQuestion.append(question3, firstInput3, firstLabel3, secondInput3, SecondLabel3, thirdInput3, thirdLabel3);
   
+   
 
 });
    
